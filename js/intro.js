82c472f9e70e8ea2dacafde96122798e00165899
unknown <diana@SPC10.TELE>
2016-02-02 16:05:01 -0200

    criacao .js para calcular IMC e correcao do caminho no index do css

--- js/intro.js
+++ js/intro.js
@@ -1,7 +1,13 @@
 
-  console.log("Olá Diana!");
+
 
   function exibir() {
     var idade = prompt("Qual é a sua idade?");
     alert(idade);
+  }
+
+//exibe o conteudo no console (se não houver uma saida aparece undefined), pode ser criado direto no console ex. var leo= "sim"
+  function usandoConsole(){
+    console.log("Olá Diana!");
+    console.log("Seja bem vinda!");
   }
