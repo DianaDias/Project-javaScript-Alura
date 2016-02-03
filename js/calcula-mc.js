bfe08a13f3366c55c7cf45e1edc5a070f50842c3
unknown <diana@SPC10.TELE>
2016-02-03 13:28:53 -0200

    Otimizando javaScript calcula-mc

--- js/calcula-mc.js
+++ js/calcula-mc.js
@@ -1,13 +1,22 @@
   function calculaImc() {
-    var peso = document.getElementById("peso-1").textContent;
+    var peso = document.getElementById("peso-1").textContent; //seleciono o elemento e pego o valor do campo
     var altura = document.getElementById("altura-1").textContent;
- var alura = document.getElementById('id')
-      if(altura != 0 && peso != 0){
-        var imc = peso / (altura * altura);
-        var tdImc = document.getElementById("imc-1");
-        tdImc.textContent = imc;
-        console.log(imc);
-      } else {
-        console("Não foi possivel calcular, pois a altura ou o peso estão com valor igual a zero!")
-      }
+    var nome = document.getElementById("nome-1");
+    //atribuindo valores ao paciente;
+    var paciente = {
+      "nome": nome,
+      "peso": peso,
+      "altura": altura
+    };
+
+    if (paciente.altura != 0 && paciente.peso != 0) {
+      var imc = paciente.peso / (paciente.altura * paciente.altura);
+      var tdImc = document.getElementById("imc-1"); //seleciono o elemento
+      tdImc.textContent = imc; //modifico o valor do imc-1 com o valor gerado pela variavel imc.
+      //  var nomeex = "Julia";
+      //  paciente.nome.textContent = nomeex;
+      console.log(imc);
+    } else {
+      console("Não foi possivel calcular, pois a altura ou o peso estão com valor igual a zero!")
+    }
   }
